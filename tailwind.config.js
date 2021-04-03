f889e6a26a7df7076ab31c762f60e5cb62cd8e16
Lord Sayur <umar_317@hotmail.com>
2021-04-03 10:51:14 +0800

    🔧 add tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      first: 'var(--first-color)',
+      second: 'var(--second-color)',
+      third: 'var(--third-color)',
+      fourth: 'var(--fourth-color)',
+      primary: 'var(--primary-color)',
+      secondary: 'var(--secondary-text-color)',
+      divider: 'var(--divider-color)',
+      icon: 'var(--icon-color)',
+    },
   },
   variants: {
     extend: {},
